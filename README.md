53aa7558603d2e1c8656a54ba4a3568ca9f842ec
cc004 <1176321897@qq.com>
2024-07-19 15:07:22 +0800

    intial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pcrdapi
+
 竞技场无apikey查询 python纯享版
+兼容绝大多数魔改版竞技场查询
+
+## 使用方法
+1. pcrdapi文件夹放入modules文件夹中
+2. `__bot__.py`中启用模块增加pcrdapi
---
+++ __init__.py
@@ -0,0 +1,59 @@
+from hoshino import aiorequests
+from time import time
+from json import dumps, loads
+from random import choices
+from . import pcrdapi
+
+old_post = aiorequests.post
+
+_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
+    "Referer": "https://pcrdfans.com/",
+    "Origin": "https://pcrdfans.com",
+    "Accept": "*/*",
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": "",
+    "Host": "api.pcrdfans.com",
+}
+
+def _getNonce():
+    return ''.join(choices("0123456789abcdefghijklmnopqrstuvwxyz", k=16))
+
+def _getTs():
+    return int(time())
+
+def _dumps(x):
+    return dumps(x, ensure_ascii=False).replace(' ', '')
+
+async def post(url, data=None, json=None, **kwargs):
+    if url != 'https://api.pcrdfans.com/x/v1/search':
+        return await old_post(url, data, json, **kwargs)
+   
+    if data is not None:
+        if isinstance(data, bytes):
+            json = loads(data.decode('utf8'))
+        elif isinstance(data, str):
+            json = loads(data)
+    
+    assert json is not None
+    
+    data = {
+        "def": json['def'],
+        "language": json.get('language', 0),
+        "nonce": _getNonce(),
+        "page": json['page'],
+        "region": json['region'],
+        "sort": json['sort'],
+        "ts": _getTs()
+    }
+    data['_sign'] = pcrdapi.sign(_dumps(data), data['nonce'])
+    kwargs['headers'] = _headers
+    return await old_post(url, json=None, data=_dumps(data).encode('utf8'), **kwargs)
+
+aiorequests.post = post
+
+try:
+    from hoshino.modules.priconne.arena import arena
+    arena.__get_auth_key = lambda: ''
+except:
+    pass
